0e26301b096982040c7f526f958c377c80dbb31f
ludivineConstanti <ludivine.constanti@outlook.fr>
2021-04-27 20:52:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  console.log('hello');
+  return (
+    <div className="App">
+      <h1>Welcome!</h1>
+
+      <p>
+        My name is Ludivine Constanti, I am a versatile, multilingual, French
+        Art Director developer.
+      </p>
+    </div>
+  );
+}
+
+export default App;
