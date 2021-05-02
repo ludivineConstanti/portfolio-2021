88c41b31f6d3e0dcd214595c21dd101c9526d858
Ludivine Constanti <ludivine.constanti@outlook.fr>
2021-05-02 14:11:00 +0200

    Delete src/components/pages/Dino404/_helpers directory

--- src/components/pages/Dino404/_helpers/materials.js
+++
@@ -1,9 +0,0 @@
-// import * as THREE from "three";
-
-// const red = new THREE.Color(0xef4239).convertSRGBToLinear();
-// const blue = new THREE.Color(0x4284f7).convertSRGBToLinear();
-// const green = new THREE.Color(0x31ab52).convertSRGBToLinear();
-// const yellow = new THREE.Color(0xffbd08).convertSRGBToLinear();
-// const white = new THREE.Color(0xffffff).convertSRGBToLinear();
-
-export const redMat = () => <meshStandardMaterial color={"red"} />;
